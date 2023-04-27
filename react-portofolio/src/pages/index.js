2082610d6317b4c151c7562ec5e64f1f3032b73f
Eric Hong <eric.hong@intelicare.com.au>
2023-04-27 14:54:39 +0800

    Update index.js

--- react-portofolio/src/pages/index.js
+++ react-portofolio/src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
                 <BsFillMoonStarsFill className='cursor-pointer text-2xl' />
               </li>
               <li>
-                <a className='bg-cyan-500 text-white px-4 py-2 rounded-md ml-8'>
+                <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'>
                   Resume
                 </a>
               </li>
